3ddde2ce8982795b9b6e365e2e99f673042d1018
ElenaAntoshkina <163022381+ElenaAntoshkina@users.noreply.github.com>
2024-04-17 21:34:01 +0400

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,69 +1,72 @@
 --считаем количество покупателей через их id
-SELECT 
-	count(customer_id) AS customers_count
+SELECT count(customer_id) AS customers_count
 FROM customers;
 
 --считаем топ 10 продацов с наибольшей выручкой:
-with seller as (
-    select
+WITH seller AS (
+    SELECT
         employee_id,
-        concat(e.first_name, ' ', e.last_name) as seller
-    from employees as e)
-select
+        concat(e.first_name, ' ', e.last_name) AS seller
+    FROM employees AS e
+)
+
+SELECT
     se.seller,
-    count(s.sales_id) as operations,
-    floor(sum(s.quantity * p.price)) as income
-from sales as s
-inner join seller as se
-    on s.sales_person_id = se.employee_id
-inner join products as p
-    on s.product_id = p.product_id
-group by se.seller
-order by floor(sum(s.quantity * p.price)) desc
-limit 10;
+    count(s.sales_id) AS operations,
+    floor(sum(s.quantity * p.price)) AS income
+FROM sales AS s
+INNER JOIN seller AS se
+    ON s.sales_person_id = se.employee_id
+INNER JOIN products AS p
+    ON s.product_id = p.product_id
+GROUP BY se.seller
+ORDER BY floor(sum(s.quantity * p.price)) DESC
+LIMIT 10;
 
 --выводим продавцов, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам.
-with seller as (
-    select
-        concat(e.first_name, ' ', e.last_name) as name,
-        floor (avg(s.quantity * p.price)) as average_income,
-        floor (sum(s.quantity * p.price)) as income
-    from sales as s
-    inner join products as p
-        on s.product_id = p.product_id
-    inner join employees as e
-        on s.sales_person_id = e.employee_id
-    group by concat(e.first_name, ' ', e.last_name))
-select
-    se.name as seller,
+WITH seller AS (
+    SELECT
+        concat(e.first_name, ' ', e.last_name) AS name,
+        floor(avg(s.quantity * p.price)) AS average_income,
+        floor(sum(s.quantity * p.price)) AS income
+    FROM sales AS s
+    INNER JOIN products AS p
+        ON s.product_id = p.product_id
+    INNER JOIN employees AS e
+        ON s.sales_person_id = e.employee_id
+    GROUP BY concat(e.first_name, ' ', e.last_name)
+)
+
+SELECT
+    se.name AS seller,
     se.average_income
-from seller as se
-where se.average_income < (select avg(average_income) from seller)
-order by se.average_income;
+FROM seller AS se
+WHERE se.average_income < (SELECT avg(average_income) FROM seller)
+ORDER BY se.average_income;
 --выводим данные по выручке по каждому продавцу и дню недели
-with tab as (
-    select
-        concat(e.first_name, ' ', e.last_name) as seller,
-        to_char(s.sale_date, 'day') as day_of_week,
-        to_char(s.sale_date, 'ID') as num_of_week,
-        floor(sum(s.quantity * p.price)) as income
-    from sales as s
-    inner join employees as e
-        on s.sales_person_id = e.employee_id
-    inner join products as p
-        on s.product_id = p.product_id
-    group by
+WITH tab AS (
+    SELECT
+        concat(e.first_name, ' ', e.last_name) AS seller,
+        to_char(s.sale_date, 'day') AS day_of_week,
+        to_char(s.sale_date, 'ID') AS num_of_week,
+        floor(sum(s.quantity * p.price)) AS income
+    FROM sales AS s
+    INNER JOIN employees AS e
+        ON s.sales_person_id = e.employee_id
+    INNER JOIN products AS p
+        ON s.product_id = p.product_id
+    GROUP BY
         concat(e.first_name, ' ', e.last_name),
         to_char(s.sale_date, 'day'),
-        to_char(s.sale_date, 'ID'))
-select
+        to_char(s.sale_date, 'ID')
+)
+
+SELECT
     seller,
     day_of_week,
     income
-from tab
-order by num_of_week, seller;
-
-
+FROM tab
+ORDER BY num_of_week, seller;
 --считаем количество покупателей в разных возрастных группах
 with tab_age as (
     select
@@ -72,7 +75,9 @@ with tab_age as (
             when age >= 26 and age <= 40 then '26-40'
             when age >= 41 then '40+'
         end as age_category
-    from customers)
+    from customers
+)
+
 select
     age_category,
     count(age_category) as age_count
@@ -90,7 +95,9 @@ with tab as (
     from sales as s
     inner join products as p
         on s.product_id = p.product_id
-    group by s.customer_id, to_char(s.sale_date, 'YYYY-MM'))
+    group by s.customer_id, to_char(s.sale_date, 'YYYY-MM')
+)
+
 select
     selling_month,
     count(customer_id) as total_customers,
@@ -117,7 +124,9 @@ with tab as (
     inner join employees as e
         on s.sales_person_id = e.employee_id
     inner join products as p
-        on s.product_id = p.product_id)
+        on s.product_id = p.product_id
+)
+
 select
     customer,
     sale_date,
